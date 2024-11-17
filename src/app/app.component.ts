cbfceb4258643ab7c5938759c848e9e766e1ef95
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-17 18:14:21 +0100

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ParticlesComponent } from './particles/particles.component';
 import { GridComponent } from './grid/grid.component';
+import { HeaderComponent } from './core/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ParticlesComponent, GridComponent],
+  imports: [RouterOutlet, ParticlesComponent, GridComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
