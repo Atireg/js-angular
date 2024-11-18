5071a34c40b4aa9f2500a195bd1c6f595e25e2de
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-18 18:47:46 +0100

    integrated a loader

--- src/app/main/grid/grid.component.ts
+++ src/app/main/grid/grid.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { CubeComponent } from '../cube/cube.component';
 import { ApiService } from '../../api.service';
 import { Post } from '../../types/post';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [CubeComponent],
+  imports: [CubeComponent, LoaderComponent],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
 
 export class GridComponent implements OnInit {
   posts: Post[] = [];
+  isLoading = true;
   constructor(private apiService: ApiService){}
 
   ngOnInit(): void {
     this.apiService.getPosts().subscribe((posts) => {
       this.posts = posts;
+      this.isLoading = false;
     })
   }
 }
