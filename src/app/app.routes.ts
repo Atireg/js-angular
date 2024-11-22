a649629ac6f33f75702a55fc5c0622b823fa7812
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-22 10:03:38 +0100

    added home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    //TODO: fix redirect to 404
+    { path: '**', redirectTo: '/404' },
+    { path: '404', component: ErrorComponent }
+];
