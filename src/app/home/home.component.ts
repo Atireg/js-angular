e8599e6fcead915adf132406b7450030b6d10be0
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-24 17:20:06 +0100

    took out a welcome message as partial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { WelcomeMsgComponent } from '../shared/welcome-msg/welcome-msg.component';
+import { UserService } from '../user/user.service';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, WelcomeMsgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
 
+  constructor(private userService: UserService){
+
+  }
 }
