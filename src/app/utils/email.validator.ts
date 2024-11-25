71504c3daed68f39e004ca0fb6778f5a5e72291a
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-25 14:54:48 +0100

    added error messages/ validations for the password in the login form

--- src/app/utils/email.validator.ts
+++ src/app/utils/email.validator.ts
@@ -2,9 +2,7 @@ import { ValidatorFn } from "@angular/forms";
 
 export function emailValidator(domains: string[]):ValidatorFn{
     const domainString = domains.join('|');
-    // const regExp = new RegExp(`[A-Za-z0-9]{6,}+@gmail\.(${domainString})`)
-    const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainString})`);
-    // console.log({regExp});
+    const regExp = new RegExp(`[A-Za-z0-9]{6,}@gmail\.(${domainString})`);
     
     return (control) => {
         const isInvalid = control.value === '' || regExp.test(control.value)
