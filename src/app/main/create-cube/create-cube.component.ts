3c73aedacd4fabc08ffe0962d3f958e701fd8bfa
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-28 06:43:04 +0100

    added validation & errors display to the create cube form

--- src/app/main/create-cube/create-cube.component.ts
+++ src/app/main/create-cube/create-cube.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-cube',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-cube.component.html',
   styleUrl: './create-cube.component.css'
 })
 export class CreateCubeComponent {
   constructor(private apiService: ApiService){}
 
-  addCube(event: Event, inputSize: string){
-    event.preventDefault();
-    console.log(inputSize);
+  addCube(form: NgForm ){
+    console.log(form.invalid);
+    if (form.invalid){
+      return
+    }
+    console.log(form.value);
+    
     // this.apiService.createCube(inputSize).subscribe(data => {
     //   console.log(data);
     // })
