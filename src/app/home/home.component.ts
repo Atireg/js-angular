4ced25fee73485a12f54b8e07a70e935b3b46e3f
Atireg <134270279+Atireg@users.noreply.github.com>
2024-11-29 16:40:35 +0100

    added a new component with a first Rive animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { WelcomeMsgComponent } from '../shared/welcome-msg/welcome-msg.component';
 import { UserService } from '../user/user.service';
+import { WelcomeCubeComponent } from '../shared/welcome-cube/welcome-cube.component';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, WelcomeMsgComponent],
+  imports: [RouterLink, WelcomeMsgComponent, WelcomeCubeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/shared/welcome-cube/welcome-cube.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { Rive } from 'rive-js';
+
+@Component({
+  selector: 'app-welcome-cube',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-cube.component.html',
+  styleUrl: './welcome-cube.component.css'
+})
+export class WelcomeCubeComponent implements AfterViewInit{
+  @ViewChild('riveCanvas') canvasRef!: ElementRef;
+  private rive!: Rive;
+
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement;
+
+    this.rive = new Rive({
+      canvas: canvas,
+      src: 'cube-rolling.riv', 
+      autoplay: true,
+      onLoad: () => {
+        console.log('Rive animation loaded');
+      }
+    });
+  }
+
+  playAnimation() {
+    this.rive?.play();
+  }
+
+  pauseAnimation() {
+    this.rive?.pause();
+  }
+}
