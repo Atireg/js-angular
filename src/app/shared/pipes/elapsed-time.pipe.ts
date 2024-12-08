a8173a861a05e7cab6f4d02a403806aefc8cdd2f
Atireg <134270279+Atireg@users.noreply.github.com>
2024-12-08 08:43:43 +0100

    edited some styling in the home page

--- src/app/shared/pipes/elapsed-time.pipe.ts
+++ src/app/shared/pipes/elapsed-time.pipe.ts
@@ -8,7 +8,7 @@ import moment from 'moment';
 export class ElapsedTimePipe implements PipeTransform {
 
   transform(date: string, ...args: unknown[]): unknown {
-    return moment(date).fromNow();
+    return moment.utc(date).local().fromNow();
   }
 
 }
