f15c03f9dc96f9a6c3a64c7a8b9e1d8f2959ba4b
Atireg <134270279+Atireg@users.noreply.github.com>
2024-12-08 16:21:05 +0100

    included a Rive animation in the home view (!loggedIn))

--- src/app/shared/welcome-msg/welcome-msg.component.ts
+++ src/app/shared/welcome-msg/welcome-msg.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProfileDetails } from '../../types/user';
 import { UserService } from '../../user/user.service';
+import { Rive } from '@rive-app/canvas'
 
 @Component({
   selector: 'app-welcome-msg',
@@ -10,8 +11,10 @@ import { UserService } from '../../user/user.service';
   templateUrl: './welcome-msg.component.html',
   styleUrl: './welcome-msg.component.css'
 })
-export class WelcomeMsgComponent implements OnInit{
+export class WelcomeMsgComponent implements OnInit, AfterViewInit {
   @Input('isLoggedIn')isLoggedIn = false;
+
+  @ViewChild('riveCanvas', { static: false }) riveCanvas!: ElementRef<HTMLCanvasElement>;
 
   profileDetails: ProfileDetails = {
     username: '',
@@ -26,6 +29,25 @@ export class WelcomeMsgComponent implements OnInit{
       this.profileDetails = { username, email };
     }
     
+  }
+
+  ngAfterViewInit() {
+    if (this.riveCanvas?.nativeElement) {
+      try {
+        new Rive({
+          src: './cube-animation.riv',
+          canvas: this.riveCanvas.nativeElement,
+          autoplay: true,
+          onLoad: () => {
+            console.log('Rive animation loaded successfully');
+          },
+        });
+      } catch (error) {
+        console.error('Error initializing Rive:', error);
+      }
+    } else {
+      console.error('Canvas element is undefined.');
+    }
   }
   
 }
