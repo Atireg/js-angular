1c8ac8dec1714684e71f41ca4812e9a27601a1b9
Atireg <134270279+Atireg@users.noreply.github.com>
2024-12-15 09:35:02 +0100

    cleaning up styling

--- backend/router/users.js
+++ backend/router/users.js
@@ -6,4 +6,4 @@ const { auth } = require('../utils');
 router.get('/profile', auth(),authController.getProfileInfo);
 router.put('/profile', auth(),authController.editProfileInfo);
 
-module.exports = routerd
+module.exports = router
